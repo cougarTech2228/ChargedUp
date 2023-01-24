339af383f4065dee51ae3da89335f72241848b2d
symonssoftware <56047819+symonssoftware@users.noreply.github.com>
2023-01-24 06:40:22 -0500

    ButtonBoardManager integration
    We *still* weren't getting the correct April Tag ID passed to the DockWithAprilTagCommand due to the way the button bindings are generated at robot initialization. Had to create an overloaded constructor in the DockWithAprilTagCommand class to pass the ButtonBoardManager instance into it. Then, from the DockWithAprilTagCommand class, call the getAprilTagID method of the ButtonBoardManager class to get the correct ID. Crazy stupid workaround to get these button bindings to work correctly.

--- src/main/java/frc/robot/commands/DockWithAprilTagCommand.java
+++ src/main/java/frc/robot/commands/DockWithAprilTagCommand.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
+import frc.robot.utils.ButtonBoardManager;
 import frc.robot.utils.DockWithAprilTag;
 import frc.robot.Constants;
 
@@ -11,11 +12,11 @@ public class DockWithAprilTagCommand extends CommandBase {
     private double m_aprilTagId;
     private boolean m_isCameraForward;
     private boolean m_useGryoForPitchCorrection;
+    private ButtonBoardManager m_buttonBoardManager;
 
     private Runnable m_dockWithAprilTagRunnable;
     private Thread m_dockWithAprilTagThread;
 
-    /** Creates a new ThreadedDockWithAprilTagCommand. */
     public DockWithAprilTagCommand(
             boolean isCameraForward,
             boolean useGryoForPitchCorrection) {
@@ -23,31 +24,52 @@ public class DockWithAprilTagCommand extends CommandBase {
         m_useGryoForPitchCorrection = useGryoForPitchCorrection;
     }
 
+    public DockWithAprilTagCommand(
+            boolean isCameraForward,
+            boolean useGryoForPitchCorrection,
+            ButtonBoardManager buttonBoardManager) {
+        m_isCameraForward = isCameraForward;
+        m_useGryoForPitchCorrection = useGryoForPitchCorrection;
+        m_buttonBoardManager = buttonBoardManager;
+    }
+
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
 
-        // Get the correct AprilTag ID based on Position and Alliance Color
-        if (RobotContainer.getShuffleboardManager().getAutoPosition() == Constants.AutoPosition.Position1) {
-            if (DriverStation.getAlliance() == Alliance.Blue) {
-                m_aprilTagId = 6.0;
+        // If we're using this class from an Autonomous command, we will not
+        // be sending in the ButtonBoardManager. If it's being used in Teleop,
+        // then ButtonBoardManager is sent in and we can use that to get the
+        // current target April Tag ID. This 'hack' is necessary to get the
+        // button bindings in the ButtonBoardManager to work. Without this
+        // approach, we were never getting the correct April Tag ID value
+        // presumably due to how the button bindings are created at
+        // initialization.
+        if (m_buttonBoardManager == null) {
+            // Get the correct AprilTag ID based on Position and Alliance Color
+            if (RobotContainer.getShuffleboardManager().getAutoPosition() == Constants.AutoPosition.Position1) {
+                if (DriverStation.getAlliance() == Alliance.Blue) {
+                    m_aprilTagId = 6.0;
+                } else {
+                    m_aprilTagId = 1.0;
+                }
+            } else if (RobotContainer.getShuffleboardManager().getAutoPosition() == Constants.AutoPosition.Position2) {
+                if (DriverStation.getAlliance() == Alliance.Blue) {
+                    m_aprilTagId = 7.0;
+                } else {
+                    m_aprilTagId = 2.0;
+                }
+            } else if (RobotContainer.getShuffleboardManager().getAutoPosition() == Constants.AutoPosition.Position3) {
+                if (DriverStation.getAlliance() == Alliance.Blue) {
+                    m_aprilTagId = 8.0;
+                } else {
+                    m_aprilTagId = 3.0;
+                }
             } else {
-                m_aprilTagId = 1.0;
-            }
-        } else if (RobotContainer.getShuffleboardManager().getAutoPosition() == Constants.AutoPosition.Position2) {
-            if (DriverStation.getAlliance() == Alliance.Blue) {
-                m_aprilTagId = 7.0;
-            } else {
-                m_aprilTagId = 2.0;
-            }
-        } else if (RobotContainer.getShuffleboardManager().getAutoPosition() == Constants.AutoPosition.Position3) {
-            if (DriverStation.getAlliance() == Alliance.Blue) {
-                m_aprilTagId = 8.0;
-            } else {
-                m_aprilTagId = 3.0;
+                System.out.println("Invalid position received from Shuffleboard");
             }
         } else {
-            System.out.println("Invalid position received from Shuffleboard");
+            m_aprilTagId = m_buttonBoardManager.getAprilTagID();
         }
 
         System.out.println("Running auto dock with AprilTag command for tag ID: " + m_aprilTagId);
--- src/main/java/frc/robot/utils/ButtonBoardManager.java
+++ src/main/java/frc/robot/utils/ButtonBoardManager.java
@@ -21,64 +21,60 @@ public class ButtonBoardManager {
     public ButtonBoardManager() {
         m_joystick = new Joystick(kJoystickChannel);
     }
-    // public static Joystick m_buttonBoardManager = new Joystick(1);
 
-    public JoystickButton getJoystickButton1() {
+    private JoystickButton getJoystickButton1() {
         return new JoystickButton(m_joystick, 1);
     }
 
-    public JoystickButton getJoystickButton2() {
+    private JoystickButton getJoystickButton2() {
         return new JoystickButton(m_joystick, 2);
     }
 
-    public JoystickButton getJoystickButton3() {
+    private JoystickButton getJoystickButton3() {
         return new JoystickButton(m_joystick, 3);
     }
 
-    public JoystickButton getJoystickButton4() {
+    private JoystickButton getJoystickButton4() {
         return new JoystickButton(m_joystick, 4);
     }
 
-    public JoystickButton getJoystickButton5() {
+    private JoystickButton getJoystickButton5() {
         return new JoystickButton(m_joystick, 5);
     }
 
-    public JoystickButton getJoystickButton6() {
+    private JoystickButton getJoystickButton6() {
         return new JoystickButton(m_joystick, 6);
     }
 
-    public JoystickButton getJoystickButton7() {
+    private JoystickButton getJoystickButton7() {
         return new JoystickButton(m_joystick, 7);
     }
 
-    public JoystickButton getJoystickButton8() {
+    private JoystickButton getJoystickButton8() {
         return new JoystickButton(m_joystick, 8);
     }
 
-    public JoystickButton getJoystickButton9() {
+    private JoystickButton getJoystickButton9() {
         return new JoystickButton(m_joystick, 9);
     }
 
-    public JoystickButton getJoystickButton10() {
+    private JoystickButton getJoystickButton10() {
         return new JoystickButton(m_joystick, 10);
     }
 
-    public JoystickButton getJoystickButton11() {
+    private JoystickButton getJoystickButton11() {
         return new JoystickButton(m_joystick, 11);
     }
 
-    public JoystickButton getJoystickButton12() {
+    private JoystickButton getJoystickButton12() {
         return new JoystickButton(m_joystick, 12);
     }
 
-    public void setPosition(Constants.AutoPosition position) {
+    private void setPosition(Constants.AutoPosition position) {
 
         System.out.println("setPosition" + position);
 
         m_gridPosition = position;
-    }
-
-    public double getAprilTagID() {
 
         if (DriverStation.getAlliance() == Alliance.Blue) {
             if (m_gridPosition == Constants.AutoPosition.Position1) {
@@ -105,39 +101,48 @@ public class ButtonBoardManager {
         }
 
         System.out.println("Setting April Tag: " + m_aprilTagID);
+    }
 
+    public double getAprilTagID() {
         return m_aprilTagID;
     }
 
     public void configureButtonBindings() {
 
+        // !! Robot MUST BE ENABLED for these commands to work !!
+
         // Bottom Three Red Buttons used to place game piece
-        getJoystickButton1().onTrue(new SequentialCommandGroup(new PrintCommand("High Left Cone")));
+        getJoystickButton1().onTrue(new SequentialCommandGroup(new PrintCommand("High Left Cone"),
+                new InstantCommand(() -> RobotContainer.getDrivetrainSubsystem().setPathPlannerDriving(false)),
+                new DockWithAprilTagCommand(false, true, this),
+                new InstantCommand(() -> RobotContainer.getDrivetrainSubsystem().setPathPlannerDriving(true))));
+
         getJoystickButton2().onTrue(new SequentialCommandGroup(new PrintCommand("High Cube")));
-        getJoystickButton3().onTrue(new SequentialCommandGroup(new PrintCommand("Hifh Right Cone")));
+
+        getJoystickButton3().onTrue(new SequentialCommandGroup(new PrintCommand("High Right Cone")));
 
         // Middle Three Red Buttons used to place game piece
         getJoystickButton4().onTrue(new SequentialCommandGroup(new PrintCommand("Middle Left Cone")));
+
         getJoystickButton5().onTrue(new SequentialCommandGroup(new PrintCommand("Middle Cube")));
+
         getJoystickButton6().onTrue(new SequentialCommandGroup(new PrintCommand("Middle Right Cone")));
 
         // Top Three Red Buttons used to place game piece
         getJoystickButton7().onTrue(new SequentialCommandGroup(new PrintCommand("Low Left Cone")));
-        getJoystickButton8().onTrue(new SequentialCommandGroup(new PrintCommand("Low Cube" )));
+
+        getJoystickButton8().onTrue(new SequentialCommandGroup(new PrintCommand("Low Cube")));
+
         getJoystickButton9().onTrue(new SequentialCommandGroup(new PrintCommand("Low Right Cone")));
 
         // Three Blue Buttons to set the position based of Alliance
-        // MUST BE ENABLED
         getJoystickButton10().onTrue(new SequentialCommandGroup(new PrintCommand("Setting Position 1"),
-                new InstantCommand(() -> setPosition(Constants.AutoPosition.Position1)),
-                new InstantCommand(() -> getAprilTagID())));
+                new InstantCommand(() -> setPosition(Constants.AutoPosition.Position1))));
+
         getJoystickButton11().onTrue(new SequentialCommandGroup(new PrintCommand("Setting Position 2"),
-                new InstantCommand(() -> setPosition(Constants.AutoPosition.Position2)),
-                new InstantCommand(() -> getAprilTagID())));
+                new InstantCommand(() -> setPosition(Constants.AutoPosition.Position2))));
+
         getJoystickButton12().onTrue(new SequentialCommandGroup(new PrintCommand("Setting Position 3"),
-                new InstantCommand(() -> setPosition(Constants.AutoPosition.Position3)),
-                new InstantCommand(() -> RobotContainer.getDrivetrainSubsystem().setPathPlannerDriving(false)),
-                new DockWithAprilTagCommand(false, true),
-                new InstantCommand(() -> RobotContainer.getDrivetrainSubsystem().setPathPlannerDriving(true))));
+                new InstantCommand(() -> setPosition(Constants.AutoPosition.Position3))));
     }
 }
